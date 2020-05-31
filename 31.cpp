10737e1d89d804bedc9ab4c77b76530049e8254f
garvit1608 <garvit1608@gmail.com>
2020-05-31 21:40:17 +0530

    Edit Distance

---
+++ 31.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+private:
+  int findMin(int a, int b, int c)
+  {
+    return min(a, min(b, c));
+  }
+
+public:
+  int minDistance(string word1, string word2)
+  {
+    int m = word1.length();
+    int n = word2.length();
+    int dp[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++)
+    {
+      for (int j = 0; j <= n; j++)
+      {
+        if (i == 0)
+          dp[i][j] = j;
+        else if (j == 0)
+          dp[i][j] = i;
+        else if (word1[i - 1] == word2[j - 1])
+          dp[i][j] = dp[i - 1][j - 1];
+        else
+          dp[i][j] = 1 + findMin(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]);
+      }
+    }
+
+    return dp[m][n];
+  }
+};
